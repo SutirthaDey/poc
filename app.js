dd58c7585b53cb54dcde2c62d7d68e9f12e7407e
SutirthaDey <deysutirtha31@gmail.com>
2022-09-22 23:41:01 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const dotenv = require('dotenv');
+const Todo = require('./models/todos');
+const sequelize = require('./utils/database');
+const server = require('http').createServer(app);
+
+dotenv.config();
+
+// requring socket io
+const io = require('socket.io')(server,{
+        cors: {
+            origin: "http://localhost:3001",
+        },
+})
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+app.get('/',(req,res,next)=>{
+    res.sendFile(path.join(__dirname,'public','index.html'));
+})
+
+// creating a connection
+
+io.on("connection", socket => {
+    
+    socket.on("add", async(data)=>{
+        const message = await Todo.create({
+            message:data
+        })
+        io.emit('response', message.id,message.message);
+    })
+
+    socket.on('fetch', async()=>{
+        const messages = await Todo.findAll({
+            attribures: ['id','message']
+    })
+        socket.emit('show',messages);
+    })
+
+    socket.on('delete', async(id)=>{
+        await Todo.destroy({
+            where: {id:id}
+        })
+
+        io.emit('remove', id);
+    })
+})
+
+
+async function runServer(){
+    await sequelize.sync();
+    server.listen(3001, ()=>{
+        console.log("Server running!");
+    })
+}
+
+runServer();
---
+++ public/index.js
@@ -0,0 +1,55 @@
+const message = document.querySelector('form');
+const showList = document.getElementById("showList");
+
+const socket = io("http://localhost:3001");
+
+socket.on('connection');
+
+socket.emit('fetch');
+
+//showing the messages
+
+socket.on('show', (messages)=>{
+    messages.forEach(({id,message}) => {
+        displayMessage(id,message);
+    });
+})
+
+socket.on('response',(id,message)=>{
+    displayMessage(id,message)
+})
+
+socket.on('remove',(id)=>{
+    removeMessage(id);
+})
+
+message.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const messageInput = e.target.message;
+    
+    if(messageInput.value=="") return;
+
+    // adding the messages
+    socket.emit("add",messageInput.value);
+
+    messageInput.value = "";
+})
+
+showList.addEventListener('click',(e)=>{
+    if(e.target.innerText == 'Delete')
+    {
+     const targetDiv = e.target.parentElement;
+     socket.emit('delete',targetDiv.id);
+    }
+})
+
+function displayMessage(id,message)
+{
+ showList.innerHTML += `<div id=${id} class="items"><h2>${message}</h2><button>Delete</button></div>`
+}
+
+function removeMessage(id){
+    const target = document.getElementById(id);
+    target.remove();
+}
+
